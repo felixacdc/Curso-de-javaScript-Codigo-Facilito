0e731347658b43c829a30a98ec8c2428d55131fa
felixacdc <pfmata01@gmail.com>
2016-06-20 23:38:07 -0600

    [add] 1.- Curso javaScript - Introducción, Hola Mundo y Variables

---
+++ Introduccion/script.js
@@ -0,0 +1,26 @@
+/*
+Formas de despliegue
+*/
+
+// Consola
+console.log("Hola Mundo!!");
+// Alerta
+alert("Hola Mundo!!");
+// DOM
+document.querySelector("body").innerHTML = "Hola Mundo!!!";
+
+// Declaracion de variables
+var nombre;
+nombre = "Felix";
+
+var edad = 22;
+
+var precio = 12.5;
+
+// Se puede crear variables sin necesidad de la palabra var
+canal = "Codigo Facilito";
+
+console.log(canal);
+
+// Concatenacion
+document.querySelector("body").innerHTML = "Hola " + nombre + "!!!";
