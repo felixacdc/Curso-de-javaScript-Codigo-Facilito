b39b3e0d4217712245c5db3cf9836c32ee3f93e6
felixacdc <pfmata01@gmail.com>
2016-06-21 14:26:14 -0600

    [add] 4.- Curso javaScript - Condiciones

---
+++ 4.- Curso javaScript - Condiciones/script.js
@@ -0,0 +1,24 @@
+if (true) {
+    console.log("La condicion fue verdadera");
+}
+
+// negacion
+if (!false) {
+    console.log("La condicion fue verdadera");
+}
+
+var numero_uno = 22;
+
+var numero_dos = 30;
+
+if ( numero_uno > numero_dos) {
+    console.log("Entre al bloque");
+}
+/*Operadores Logicos*/
+// || OR
+// && AND
+// == === IGUAL A
+// != !== DIFERENTE A
+// ! NEGACION
+// < MENOR QUE
+// > MAYOR QUE
