f8f7e091d7ea99fe6fb5245047c0bcafa19af0d8
felixacdc <pfmata01@gmail.com>
2016-06-20 23:53:11 -0600

    [add] 2.- Curso javaScript - Números y operaciones aritméticas

---
+++ NumerosYOperacionesArit/script.js
@@ -0,0 +1,40 @@
+var edad = 19;
+
+// Suma
+console.log(edad + 3);
+
+// Incremento
+edad++;
+
+var punto_flotante = 12.5;
+
+// Se puede sumar enteros con flotantes
+console.log(edad + punto_flotante);
+
+/*
+Operadores aritmeticos
+Divicion = /
+Multiplicacion = *
+Suma = +
+Resta = -
+Modulo = %
+*/
+console.log(edad - punto_flotante);
+console.log(edad * 2);
+console.log(edad / 2);
+console.log(edad % 3);
+
+// Potencia
+console.log(Math.pow(2,3));
+// Redondiar
+console.log(Math.round(0.4));
+// Redondea al entero arriba mas sercano
+console.log(Math.ceil(0.1));
+// Redondea al entero mas abajo mas sercano
+console.log(Math.floor(0.9));
+// PI
+console.log(Math.PI);
+// Numeros Aleatorios entre 0 y 1
+console.log(Math.random());
+// Raiz Cuadrada
+console.log(Math.sqrt(36));
