42c812223e52297c80dc6a319eaf8254241b1527
felixacdc <pfmata01@gmail.com>
2016-06-24 14:25:32 -0600

    [add] 23.- Curso javaScript - Practica MVC parte 4

--- 23.- Curso javaScript - Practica MVC parte 1/script.js
+++ 23.- Curso javaScript - Practica MVC parte 1/script.js
@@ -29,6 +29,20 @@
 })();
 
 (function() {
+    self.Ball = function(x, y, radius, board) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed_y = 0;
+        this.speed_x = 3;
+        this.board = board;
+
+        board.ball = this;
+        this.kind = "circle";
+    }
+})();
+
+(function() {
     self.Bar = function (x, y, width, height, board) {
         this.x = x;
         this.y = y;
@@ -72,12 +86,19 @@
     }
 
     self.BoardView.prototype = {
+        clean: function() {
+            this.ctx.clearRect(0, 0, this.board.width, this.board.height);
+        },
         draw: function() {
             for (var i = this.board.elements.length -1; i >= 0; i--) {
                 var element = this.board.elements[i];
 
                 draw(this.ctx, element);
             }
+        },
+        play: function() {
+            this.clean();
+            this.draw();
         }
     }
 
@@ -86,40 +107,55 @@
 
         // hasOwnProperty: verifica si mi objeto tiene una propiedad determinada en este caso kind
 
-        if ( element !== null && element.hasOwnProperty("kind")) {
+        // if ( element !== null && element.hasOwnProperty("kind")) {
 
             switch(element.kind) {
                 case "rectangle":
                     // permite dibujar un cuadrado en canvas
                     ctx.fillRect(element.x, element.y, element.width, element.height);
                     break;
+                case "circle":
+                    ctx.beginPath();
+                    // los valor 0 y 7 son necesarios para formar el circulo 
+                    ctx.arc(element.x, element.y, element.radius, 0, 7);
+                    ctx.fill();
+                    ctx.closePath();
+                    break;
             }
             
-        }
+        // }
 
     }
 })();
 
 var board = new Board(800, 400);
 var bar = new Bar(20, 100, 40, 100, board);
-var bar = new Bar(735, 100, 40, 100, board);
+var bar_2 = new Bar(735, 100, 40, 100, board);
 var canvas = document.getElementById("canvas");
 var board_view = new BoardView(canvas, board);
+var ball = new Ball(350, 100, 10, board);
 
 document.addEventListener("keydown", function(ev) {
-    console.log(ev.keyCode);
+
+    ev.preventDefault();
     if( ev.keyCode == 38 ) {
         bar.up();
     } else if( ev.keyCode == 40 ) {
         bar.down();
-    }
+    } else if( ev.keyCode === 87 ) {
+        bar_2.up();
+    } else if( ev.keyCode === 83 ) {
+        bar_2.down();
+    } 
 
-    console.log(bar.toString());
 });
 
-window.addEventListener("load", main);
+/*
+window.requestAnimationFrame informa al navegador que quieres realizar una animación y solicita que el navegador programe el repintado de la ventana para el próximo ciclo de animación. El método acepta como argumento una función a la que llamar antes de efectuar el repintado.
+*/
+window.requestAnimationFrame(controller);
 
-function main() {
-
-    board_view.draw();
+function controller() {
+    board_view.play();
+    window.requestAnimationFrame(controller);
 }
