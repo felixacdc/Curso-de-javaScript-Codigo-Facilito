9570246d5d40389767eb67c4ea1abd0324e1adf4
felixacdc <pfmata01@gmail.com>
2016-06-24 20:46:53 -0600

    [add] 23.- Curso javaScript - Practica MVC parte 6

--- 23.- Curso javaScript - Practica MVC parte 1/script.js
+++ 23.- Curso javaScript - Practica MVC parte 1/script.js
@@ -39,6 +39,9 @@
         this.speed_x = 3;
         this.board = board;
         this.direction = 1;
+        this.bounce_angle = 0;
+        this.max_bounce_angle = Math.PI / 12;
+        this.speed = 3;
 
         board.ball = this;
         this.kind = "circle";
@@ -48,6 +51,28 @@
         move: function() {
             this.x += (this.speed_x * this.direction);
             this.y += (this.speed_y);
+        },
+        get width() {
+            return this.radius * 2;
+        },
+        get height() {
+            return this.radius * 2;
+        },
+        collision: function(bar) {
+            // Reacciona con una colision con una barra que recibe como parametro
+            var relative_intersect_y = ( bar.y + (bar.height / 2) ) - this.y;
+
+            var normalized_intersect_y = relative_intersect_y / (bar.height / 2);
+
+            this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
+            console.log(this.bounce_angle);
+            this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+            this.speed_x = this.speed * Math.cos(this.bounce_angle);
+
+            if (this.x > (this.board.width / 2)) 
+                this.direction = -1;
+            else 
+                this.direction = 1;
         }
     }
 })();
@@ -106,13 +131,49 @@
                 draw(this.ctx, el);
             }
         },
+        check_collisions: function() {
+            for (var i = this.board.bars.length - 1; i >= 0; i--) {
+                var bar = this.board.bars[i];
+                if ( hit(bar, this.board.ball) ) {
+                    this.board.ball.collision(bar);
+                }
+            }
+        },
         play: function() {
             if ( this.board.playing ) {
                 this.clean();
                 this.draw();
+                this.check_collisions();
                 this.board.ball.move();
             }
         }
+    }
+
+    function hit(a, b) {
+        console.log("hit");
+        // revisa si a coliciona con b
+        var hit = false;
+        // Colisiones horizontales
+        if ( b.x + b.width >= a.x && b.x < a.x + a.width ) {
+            // Colisiones verticales
+            if ( b.y + b.height >= a.y && b.y < a.y + a.height ) {
+                hit = true;
+            }
+        }
+
+        // Colision de a con b
+        if( b.x <= a.x && b.x + b.width >= a.x + a.width ) {
+            if ( b.y <= a.y && b.y + b.height >= a.y + a.height )
+                hit = true; 
+        }
+
+        // Colision de b con a
+        if ( a.x <= b.x && a.x + a.width >= b.x + b.width ) {
+            if ( a.y <= b.y && a.y + a.height >= b.y + b.height )
+                hit = true; 
+        }
+
+        return hit;
     }
 
     // Helper Method
@@ -177,9 +238,6 @@ board_view.draw();
 window.requestAnimationFrame informa al navegador que quieres realizar una animación y solicita que el navegador programe el repintado de la ventana para el próximo ciclo de animación. El método acepta como argumento una función a la que llamar antes de efectuar el repintado.
 */
 window.requestAnimationFrame(controller);
-setTimeout(function() {
-    ball.direction = -1;
-}, 3000);
 
 function controller() {
     board_view.play();
